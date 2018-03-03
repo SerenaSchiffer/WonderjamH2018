8071cd8b1e9ff87b1d163c5aceb5992fa214136e
Emile Fortin <sylfschiffer@gmail.com>
2018-03-03 00:19:40 -0500

    Ajout de Melange.MelangeValue()

--- WonderJam2018/Assets/Scripts/Melange.cs
+++ WonderJam2018/Assets/Scripts/Melange.cs
@@ -7,7 +7,7 @@ public class Melange : ScriptableObject {
     public static Ingredient[] ingredients;
 
     public Queue<Ingredient> mesIngredients;
-        
+
     private void OnEnable()
     {
         if (ingredients == null)
@@ -21,33 +21,33 @@ public class Melange : ScriptableObject {
         mesIngredients.Clear();
         int numberOfIngredients = Random.Range(1, 4);
 
-        for(int i = 0; i < numberOfIngredients; i++)
+        for (int i = 0; i < numberOfIngredients; i++)
         {
             int ingredient = Random.Range(0, ingredients.Length);
             mesIngredients.Enqueue(ingredients[ingredient]);
         }
     }
-    
+
     public void AddIngredient(Ingredient i)
     {
         mesIngredients.Enqueue(i);
     }
 
-    public Color MelangeColor() 
+    public Color MelangeColor()
     {
         List<Color> allColors = new List<Color>();
-        foreach(Ingredient i in mesIngredients)
+        foreach (Ingredient i in mesIngredients)
             allColors.Add(i.mainColor);
 
         float rSum = 0f, gSum = 0f, bSum = 0f;
-        
+
         foreach (Color c in allColors)
         {
             rSum += (c.r * 255) * (c.r * 255);
             gSum += (c.g * 255) * (c.g * 255);
             bSum += (c.b * 255) * (c.b * 255);
         }
-        
+
         rSum /= allColors.Count;
         gSum /= allColors.Count;
         bSum /= allColors.Count;
@@ -56,8 +56,19 @@ public class Melange : ScriptableObject {
         gSum = Mathf.Sqrt(gSum);
         bSum = Mathf.Sqrt(bSum);
 
-        return new Color(rSum / 255 , gSum / 255, bSum / 255, 1f);
+        return new Color(rSum / 255, gSum / 255, bSum / 255, 1f);
     }
 
+    static float[] multiplier = { 0, 1f, 1.2f, 1.5f }; 
+    public float MelangeValue()
+    {
+        float value = 0f;
 
+        foreach (Ingredient i in mesIngredients)
+            value += i.baseIngredientValue;
+
+        value *= multiplier[mesIngredients.Count];
+
+        return value;
+    }
 }
