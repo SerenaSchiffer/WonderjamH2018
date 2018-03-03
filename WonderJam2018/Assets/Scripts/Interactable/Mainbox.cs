a88c7235f6332054f6ba9ca92a8ee980d1c20520
Pierre-André Savard <pandre711@gmail.com>
2018-03-03 14:55:35 -0500

    push des bos terminé avec anim

--- WonderJam2018/Assets/Scripts/Interactable/Mainbox.cs
+++ WonderJam2018/Assets/Scripts/Interactable/Mainbox.cs
@@ -4,30 +4,32 @@ using UnityEngine;
 
 public class Mainbox : MonoBehaviour {
 
-    SpriteRenderer myRenderer;
+    Animator myAnimator;
+
     Box[] childs;
 
 	// Use this for initialization
 	void Start () {
-        myRenderer = GetComponent<SpriteRenderer>();
+        myAnimator = GetComponent<Animator>();
         childs = new Box[transform.childCount];
-        for(int i = 0; i < transform.childCount; i++)
+        for (int i = 0; i < transform.childCount; i++)
         {
             childs[i] = transform.GetChild(i).GetComponent<Box>();
         }
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		foreach(Box b in childs)
+        myAnimator.SetBool("isClosed", LookifOpened());
+	}
+
+    bool LookifOpened()
+    {
+        foreach (Box b in childs)
         {
             if (b.GetHighlight() > 0)
-            {
-                myRenderer.enabled = false;
-                break;
-            }
-            else
-                myRenderer.enabled = true;
+                return false;
         }
-	}
+            return true;
+    }
 }
