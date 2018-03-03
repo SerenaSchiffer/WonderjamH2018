fc90ba664e5cd6667b4dc8373bf7421d5f5ddfba
Emile Fortin <sylfschiffer@gmail.com>
2018-03-02 19:28:35 -0500

    Ajout des sprites de test ingr/dients

---
+++ WonderJam2018/Assets/Scripts/Ingredient.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/Ingredient")]
+public class Ingredient : ScriptableObject {
+
+    public Sprite ingredientSprite;
+    public int baseIngredientValue;
+    public Color mainColor;
+}
---
+++ WonderJam2018/Assets/Scripts/Melange.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/Melange")]
+public class Melange : ScriptableObject {
+    public static Ingredient[] ingredients;
+
+    Queue<Ingredient> mesIngredients;
+
+
+}
