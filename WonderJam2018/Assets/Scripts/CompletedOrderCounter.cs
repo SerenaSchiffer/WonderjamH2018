b43d29c9e77cbf945a1837be45be537329d6c58b
Rafaël Côté <raf87crosby@hotmail.com>
2018-03-03 01:34:40 -0500

    ajout script du comptoir de service

---
+++ WonderJam2018/Assets/Scripts/CompletedOrderCounter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompletedOrderCounter : MonoBehaviour {
+
+    [HideInInspector] public Queue<Melange> completedMelanges;
+
+	// Use this for initialization
+	void Start () {
+        completedMelanges = new Queue<Melange>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (FindMatchingClient())
+	}
+
+    public void AddMelange(Melange newCompletedMelange)
+    {
+        completedMelanges.Enqueue(newCompletedMelange);
+    }
+
+    private GameObject FindMatchingClient()
+    {
+        Client[] clients;
+        foreach (Client client in clients)
+        {
+            int cptGoodIngredient = 0;
+
+            if (client == client.melangeClient.mesIngredients.Dequeue())
+                cptGoodIngredient++;
+
+            if (completedMelanges.Count == cptGoodIngredient)
+                return client.gameObject;
+        }
+
+        return null;
+    }
+}
