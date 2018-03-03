b7e11de05dfeaa59dd5171054399bbe2fcf6ebfa
Pierre-André Savard <pandre711@gmail.com>
2018-03-02 21:00:08 -0500

    finition du UI de base et modif du git ignore

---
+++ WonderJam2018/Assets/Scripts/UIManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    [SerializeField] Text player1Text;
+    [SerializeField] Text player2Text;
+    Vector3 basePosition1, basePosition2, goal1, goal2;
+    public bool test = false;
+    public float lerpSpeed = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+        basePosition1 = player1Text.transform.position;
+        basePosition2 = player2Text.transform.position;
+        goal1 = basePosition1;
+        goal2 = basePosition2;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(test)
+        {
+            SwapUI();
+            test = false;
+            lerpSpeed = 0.01f;
+        }
+        LookPosition();
+        lerpSpeed += 0.03f;
+	}
+
+    void SwapUI()
+    {
+        goal1 = basePosition2;
+        goal2 = basePosition1;
+        basePosition1 = goal1;
+        basePosition2 = goal2;        
+    }
+
+    void LookPosition()
+    {
+        float newX1 = Mathf.Lerp(player1Text.transform.position.x, goal1.x, lerpSpeed * Time.deltaTime);
+        player1Text.transform.position = new Vector2(newX1, player1Text.transform.position.y);
+        float newX2 = Mathf.Lerp(player2Text.transform.position.x, goal2.x, lerpSpeed * Time.deltaTime);
+        player2Text.transform.position = new Vector2(newX2, player2Text.transform.position.y);
+    }
+}
