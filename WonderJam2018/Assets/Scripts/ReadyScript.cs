28675a275fdac0ab0f13146c109780613bbaf04c
Rafaël Côté <raf87crosby@hotmail.com>
2018-03-04 04:16:31 -0500

    ajout d'un main menu like et gestion des controlles multi joueurs

---
+++ WonderJam2018/Assets/Scripts/ReadyScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ReadyScript : MonoBehaviour {
+
+    [SerializeField] GameObject player1;
+    [SerializeField] GameObject player2;
+
+    bool player1Ready = false;
+    bool player2Ready = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+        {
+            player1.transform.GetChild(0).GetComponent<Text>().color = Color.green;
+            player1Ready = true;
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            player2.transform.GetChild(0).GetComponent<Text>().color = Color.green;
+            player2Ready = true;
+        }
+
+        if (player1Ready && player2Ready)
+        {
+            Debug.Log("ma mere");
+            SceneManager.LoadScene("Nichzee");
+        }
+	}
+}
